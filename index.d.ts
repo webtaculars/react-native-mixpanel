e6dd05ec28d8780c8e4f682fba1305246eb61922
abhishek gupta <ag251994@gmail.com>
2019-11-22 08:36:47 +0530

    Added remove method

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,15 @@
 declare module 'react-native-mixpanel' {
   export class MixpanelInstance {
-    constructor(apiToken?: string, optOutTrackingDefault?: boolean, trackCrashes?: boolean, automaticPushTracking?: boolean, launchOptions?: Object)
+    constructor(apiToken?: string)
 
     initialize(): Promise<void>
-    getDistinctId(): Promise<string>
+    getDistinctId(): Promise<string>  
     getSuperProperty(propertyName: string): Promise<any>
     track(event: string, properties?: Object): Promise<void>
     flush(): Promise<void>
     disableIpAddressGeolocalization(): Promise<void>
     alias(alias: string): Promise<void>
-    identify(userId: string): Promise<void>
+    identify(userId: string): Promise<void>  
     timeEvent(event: string): Promise<void>
     registerSuperProperties(properties: Object): Promise<void>
     registerSuperPropertiesOnce(properties: Object): Promise<void>
@@ -20,26 +20,22 @@ declare module 'react-native-mixpanel' {
     trackChargeWithProperties(charge: number, properties: Object): Promise<void>
     increment(property: string, by: number): Promise<void>
     union(name: string, properties: any[]): Promise<void>
-    append(name: string, properties: any[]): Promise<void>
-    clearSuperProperties(): Promise<void>
-    reset(): Promise<void>
-    showInAppMessageIfAvailable(): Promise<void>
-    optInTracking(): Promise<void>
-    optOutTracking(): Promise<void>
+    remove(name: string, properties: string): Promise<void>
+    removePushDeviceToken(deviceToken: Object): Promise<void>
+    removeAllPushDeviceTokens(): Promise<void>
+    addPushDeviceToken(token: string): Promise<void>
 
     // android only
     setPushRegistrationId(token: string): Promise<void>
-    clearPushRegistrationId(token?: string): Promise<void>
-    getPushRegistrationId(): Promise<string>
+  
+    // android only
+    clearPushRegistrationId(): Promise<void>
 
-    // iOS only
-    removePushDeviceToken(pushDeviceToken: string): Promise<void>
-    removeAllPushDeviceTokens(): Promise<void>
-    addPushDeviceToken(token: string): Promise<void>
+    reset(): Promise<void>
   }
 
   interface MixpanelAPI {
-    sharedInstanceWithToken(apiToken: string, optOutTrackingDefault?: boolean, trackCrashes?: boolean, automaticPushTracking?: boolean, launchOptions?: Object): Promise<void>;
+    sharedInstanceWithToken(apiToken: string): Promise<void>;
     getDistinctId(callback: (id?: string) => void): void;
     getSuperProperty(propertyName: string, callback: (value: any) => void): void;
     track(event: string): void;
@@ -54,29 +50,25 @@ declare module 'react-native-mixpanel' {
     initPushHandling(token: string): void;
     set(properties: Object): void;
     setOnce(properties: Object): void;
+    removePushDeviceToken(deviceToken: Object): void;
+    removeAllPushDeviceTokens(): void;
     trackCharge(charge: number): void;
     trackChargeWithProperties(charge: number, properties: Object): void;
     increment(property: string, by: number): void;
     union(name: string, properties: any[]): void;
-    append(name: string, properties: any[]): void;
-    clearSuperProperties(): void;
-    reset(): void;
-    showInAppMessageIfAvailable(): void;
-    optInTracking(): void
-    optOutTracking(): void
-
+    remove(name: string, properties: string): void;
+    addPushDeviceToken(token: string): void;
+  
     // android only
     setPushRegistrationId(token: string): void;
-    clearPushRegistrationId(token?: string): void;
-    getPushRegistrationId(callback: (token?: string) => void): void;
+  
+    // android only
+    clearPushRegistrationId(): void;
 
-    // iOS only
-    addPushDeviceToken(token: string): void;
-    removePushDeviceToken(pushDeviceToken: string): void;
-    removeAllPushDeviceTokens(): void;
+    reset(): void;
   }
 
   const mixpanelApi: MixpanelAPI;
   export default mixpanelApi;
-
+  
 }
